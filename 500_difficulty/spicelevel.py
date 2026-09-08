52bea306cec8e0fe876354c0e748f7c505decd0d
deepikaparasa6 <deepikaparasa6@gmail.com>
2026-09-08 20:45:39 +0530

    300-400 Rated Problems

---
+++ 500_difficulty/spicelevel.py
@@ -0,0 +1,5 @@
+for _ in range(int(input())):
+    x = int(input())
+    if x < 4: print('Mild')
+    elif x < 7: print('Medium')
+    else: print('Hot')
---
+++ 500_difficulty/thegift.py
@@ -0,0 +1,2 @@
+x, n, m = map(int, input().split())
+print('Yes' if n <= x+m else 'No')
