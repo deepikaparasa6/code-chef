34f23da1f5cd392a1ab532fe3ccd40517f8b8e9b
deepikaparasa6 <deepikaparasa6@gmail.com>
2026-09-13 21:05:28 +0530

    450 Rated Problems

---
+++ 500_difficulty/firstandlastdigit.py
@@ -0,0 +1,11 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+
+    last_digit = N % 10
+
+    first_digit = N
+    while first_digit >= 10:
+        first_digit //= 10
+
+    print(first_digit + last_digit)
